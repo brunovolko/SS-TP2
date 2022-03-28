f521deb83019a9df5e5ea05552c36d993a6cb704
jsackmann <jsackmann@itba.edu.ar>
2022-03-28 10:56:41 -0300

    fixed grid3d border checks

--- Simulation/src/main/java/Grid3d.java
+++ Simulation/src/main/java/Grid3d.java
@@ -27,42 +27,83 @@ public class Grid3d implements Grid {
         if(!isSomeoneAlive)
             return false;
 
-        for(int k= 0; k < grid[0][0].length; k++) // Con columna = 0, fila = 0
-            if(grid[0][0][k])
-                return false;
-        for(int k= 0; k < grid[0][0].length; k++) // Con columna = 0, fila = ultima
-            if(grid[grid.length-1][0][k])
-                return false;
-        for(int k= 0; k < grid[0][0].length; k++) // Con columna = ultima, fila = 0
-            if(grid[0][grid[0].length][k])
-                return false;
-        for(int k= 0; k < grid[0][0].length; k++) // Con columna = ultima, fila = ultima
-            if(grid[grid.length-1][grid[0].length-1][k])
-                return false;
-        for(int j= 0; j < grid[0].length; j++) // Con profundidad = 0, fila = 0
-            if(grid[0][j][0])
-                return false;
-        for(int j= 0; j < grid[0].length; j++) // Con profundidad = 0, fila = ultima
-            if(grid[grid.length-1][j][0])
-                return false;
-        for(int j= 0; j < grid[0].length; j++) // Con profundidad = ultima, fila = 0
-            if(grid[0][j][grid[0][0].length-1])
-                return false;
-        for(int j= 0; j < grid[0].length; j++) // Con profundidad = ultima, fila = ultima
-            if(grid[grid.length-1][j][grid[0][0].length-1])
-                return false;
-        for(int i= 0; i < grid.length; i++) // Con profundidad = 0, columna = 0
-            if(grid[i][0][0])
-                return false;
-        for(int i= 0; i < grid.length; i++) // Con profundidad = 0, columna = ultima
-            if(grid[i][grid[0].length-1][0])
-                return false;
-        for(int i= 0; i < grid.length; i++) // Con profundidad = ultima, columna = 0
-            if(grid[i][0][grid[0][0].length-1])
-                return false;
-        for(int i= 0; i < grid.length; i++) // Con profundidad = ultima, columna = ultima
-            if(grid[i][grid[0].length-1][grid[0][0].length-1])
-                return false;
+//        for(int k= 0; k < grid[0][0].length; k++) // Con columna = 0, fila = 0
+//            if(grid[0][0][k])
+//                return false;
+//        for(int k= 0; k < grid[0][0].length; k++) // Con columna = 0, fila = ultima
+//            if(grid[grid.length-1][0][k])
+//                return false;
+//        for(int k= 0; k < grid[0][0].length; k++) // Con columna = ultima, fila = 0
+//            if(grid[0][grid[0].length][k])
+//                return false;
+//        for(int k= 0; k < grid[0][0].length; k++) // Con columna = ultima, fila = ultima
+//            if(grid[grid.length-1][grid[0].length-1][k])
+//                return false;
+//        for(int j= 0; j < grid[0].length; j++) // Con profundidad = 0, fila = 0
+//            if(grid[0][j][0])
+//                return false;
+//        for(int j= 0; j < grid[0].length; j++) // Con profundidad = 0, fila = ultima
+//            if(grid[grid.length-1][j][0])
+//                return false;
+//        for(int j= 0; j < grid[0].length; j++) // Con profundidad = ultima, fila = 0
+//            if(grid[0][j][grid[0][0].length-1])
+//                return false;
+//        for(int j= 0; j < grid[0].length; j++) // Con profundidad = ultima, fila = ultima
+//            if(grid[grid.length-1][j][grid[0][0].length-1])
+//                return false;
+//        for(int i= 0; i < grid.length; i++) // Con profundidad = 0, columna = 0
+//            if(grid[i][0][0])
+//                return false;
+//        for(int i= 0; i < grid.length; i++) // Con profundidad = 0, columna = ultima
+//            if(grid[i][grid[0].length-1][0])
+//                return false;
+//        for(int i= 0; i < grid.length; i++) // Con profundidad = ultima, columna = 0
+//            if(grid[i][0][grid[0][0].length-1])
+//                return false;
+//        for(int i= 0; i < grid.length; i++) // Con profundidad = ultima, columna = ultima
+//            if(grid[i][grid[0].length-1][grid[0][0].length-1])
+//                return false;
+
+
+        for (int j = 0; j < grid[0].length; j++) {
+            for (int k = 0; k < grid[0][j].length; k++) {
+                if (grid[0][j][k])
+                    return false;
+            }
+        }
+        for (int j = 0; j < grid[0].length; j++) {
+            for (int k = 0; k < grid[0][j].length; k++) {
+                if (grid[grid.length-1][j][k])
+                    return false;
+            }
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int k = 0; k < grid[i][0].length; k++) {
+                if (grid[i][0][k])
+                    return false;
+            }
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int k = 0; k < grid[i][grid[i].length-1].length; k++) {
+                if (grid[i][grid[i].length-1][k])
+                    return false;
+            }
+        }
+
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j][0])
+                    return false;
+            }
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j][grid[i][j].length-1])
+                    return false;
+            }
+        }
 
 
         return true;
