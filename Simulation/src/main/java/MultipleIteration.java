2aa403aa954dcbd1fe4c8e44785636aedf54eb82
Santiago <smonjeau@itba.edu.ar>
2022-04-03 21:26:34 -0300

    add avgs and standard desv

--- Simulation/src/main/java/MultipleIteration.java
+++ Simulation/src/main/java/MultipleIteration.java
@@ -6,14 +6,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.System.exit;
+
 public class MultipleIteration {
-    private static final int QTY_OF_SIMULATIONS = 4;
+    private static final int QTY_OF_SIMULATIONS = 10;
     private static final long MAX_DURATION =  (300);
     public static void main(String[] args) throws Exception {
 
 
         File f = new File("results");
-        System.out.println(f.mkdir());
         for(File file: f.listFiles())
             file.delete();
         Config config;
@@ -41,15 +42,22 @@ public class MultipleIteration {
 
 
             int t =0;
-            accumAliveNodes.put(t,new ArrayList<>());
-            accumMaxDistances.put(t,new ArrayList<>());
-
+            accumAliveNodes.putIfAbsent(t,new ArrayList<>());
+            accumMaxDistances.putIfAbsent(t,new ArrayList<>());
+            accumAliveNodes.get(t).add(grid.getCellsAlive().size());
+            accumMaxDistances.get(t).add(grid.getMaxDistFromCenter());
 
 
 
                 while (grid.canMove()) {
                     grid.move();
                     t++;
+                    if(!accumMaxDistances.containsKey(t))
+                        accumMaxDistances.put(t,new ArrayList<>());
+                    if(!accumAliveNodes.containsKey(t))
+                        accumAliveNodes.put(t,new ArrayList<>());
+
+
                     accumAliveNodes.get(t).add(grid.getCellsAlive().size());
                     accumMaxDistances.get(t).add(grid.getMaxDistFromCenter());
 
@@ -67,22 +75,43 @@ public class MultipleIteration {
         System.out.println(accumAliveNodes.size()==accumMaxDistances.size());
         try (PrintWriter pw = new PrintWriter(dynamicOutputFile)) {
 
-            pw.write("alives,desviation,max_distance,desviation\n");
+            pw.write("alives\tdesviation\tmax_distance\tdesviation\n");
 
             for (int t = 0; t < accumAliveNodes.size(); t++) {
                 int nodesAvg = 0;
-                for(int i = 0; i < accumAliveNodes.size(); i++){
+                for(int i = 0; i < accumAliveNodes.get(t).size(); i++){
                     nodesAvg+=accumAliveNodes.get(t).get(i);
                 }
                 nodesAvg /= QTY_OF_SIMULATIONS;
 
-                double distancesAvg = 0.0;
-                for(int i = 0; i < accumMaxDistances.size(); i++){
-                    distancesAvg+=accumMaxDistances.get(t).get(i);
-                }
-                distancesAvg /= QTY_OF_SIMULATIONS;
+                double desv1 = 0.0;
 
-                pw.println(String.format("%d,%g,%g,%g",nodesAvg,0.0,distancesAvg,0.0));
+                for(int i = 0; i < accumAliveNodes.get(t).size(); i++){
+                    desv1+= (accumAliveNodes.get(t).get(i) - nodesAvg) * (accumAliveNodes.get(t).get(i) - nodesAvg);
+                }
+                desv1 /= QTY_OF_SIMULATIONS;
+
+                double distancesAvg = 0.0;
+                for(int i = 0; i < accumAliveNodes.get(t).size(); i++){
+                    desv1+= Math.pow(accumAliveNodes.get(t).get(i) - nodesAvg, 2);
+
+                }
+                desv1 = Math.sqrt(desv1/QTY_OF_SIMULATIONS);
+
+                double desv2 =0.0;
+
+                for(int i = 0; i < accumMaxDistances.get(t).size(); i++){
+                    desv2+= Math.pow(accumMaxDistances.get(t).get(i) - distancesAvg, 2);
+
+                }
+                desv2 = Math.sqrt(desv2/QTY_OF_SIMULATIONS);
+
+
+
+
+
+
+                pw.println(String.format("%d\t%g\t%g\t%g",nodesAvg,desv1,distancesAvg,desv2));
             }
         }
 
